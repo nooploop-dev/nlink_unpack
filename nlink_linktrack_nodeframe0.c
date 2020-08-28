2e25f82c93c79d714b50af75b0f75cca9f319260
samuelhsu <samuel.hsu@qq.com>
2020-08-28 21:22:24 +0800

    fix nodeframe0 data unpack bug

--- nlink_linktrack_nodeframe0.c
+++ nlink_linktrack_nodeframe0.c
@@ -4,31 +4,36 @@
 
 #include "nlink_utils.h"
 
-NLINK_PACKED(typedef struct {
-  uint8_t header[2];
-  uint16_t frame_length;
-  uint8_t role;
-  uint8_t id;
-  uint8_t reserved[4];
-  uint8_t valid_node_count;
-  // nodes...
-  // uint8_t checkSum;
-})
+NLINK_PACKED(typedef struct
+             {
+               uint8_t header[2];
+               uint16_t frame_length;
+               uint8_t role;
+               uint8_t id;
+               uint8_t reserved[4];
+               uint8_t valid_node_count;
+               // nodes...
+               // uint8_t checkSum;
+             })
 nlt_nodeframe0_raw_t;
 
 static nlt_nodeframe0_raw_t g_frame;
 
-uint8_t UnpackData(const uint8_t *data, size_t data_length) {
+uint8_t UnpackData(const uint8_t *data, size_t data_length)
+{
   if (data_length < g_nlt_nodeframe0.fixed_part_size ||
       data[0] != g_nlt_nodeframe0.frame_header ||
       data[1] != g_nlt_nodeframe0.function_mark)
     return 0;
   size_t frame_length = NLINK_PROTOCOL_LENGTH(data);
-  if (data_length < frame_length) return 0;
-  if (!NLINK_VerifyCheckSum(data, frame_length)) return 0;
+  if (data_length < frame_length)
+    return 0;
+  if (!NLINK_VerifyCheckSum(data, frame_length))
+    return 0;
 
   static uint8_t init_needed = 1;
-  if (init_needed) {
+  if (init_needed)
+  {
     memset(g_nlt_nodeframe0.result.nodes, 0,
            sizeof(g_nlt_nodeframe0.result.nodes));
     init_needed = 0;
@@ -40,11 +45,13 @@ uint8_t UnpackData(const uint8_t *data, size_t data_length) {
   g_nlt_nodeframe0.result.valid_node_count = g_frame.valid_node_count;
 
   for (size_t i = 0, address = g_nlt_nodeframe0.fixed_part_size;
-       i < g_frame.valid_node_count; ++i) {
+       i < g_frame.valid_node_count; ++i)
+  {
     const uint8_t *begin = data + address;
     size_t data_length = (size_t)(begin[2] | begin[3] << 8);
     size_t current_node_size = 4 + data_length;
-    if (address + current_node_size > frame_length - 1) {
+    if (address + current_node_size > frame_length - 1)
+    {
       printf(
           "warning: address(%zu) + current_node_size(%zu) > "
           "frame_length(%zu)-1",
@@ -57,7 +64,7 @@ uint8_t UnpackData(const uint8_t *data, size_t data_length) {
     node->role = begin[0];
     node->id = begin[1];
     node->data_length = data_length;
-    memcpy(node->data, begin + 2, data_length);
+    memcpy(node->data, begin + 4, data_length);
 
     address += current_node_size;
   }
